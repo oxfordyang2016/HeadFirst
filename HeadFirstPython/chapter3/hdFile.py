ee5733b682b43a7989d324f1810ddf2a1b3c8dd4
oxfordyang2016 <756260386@qq.com>
2015-07-31 12:20:02 +0800

    Update hdFile.py

--- HeadFirstPython/chapter3/hdFile.py
+++ HeadFirstPython/chapter3/hdFile.py
@@ -3,7 +3,7 @@ if os.path.exists('sketch.txt'):
     data = open("sketch.txt")
     for e_line in data:
         #split 当文件分割出的string个数大于1时，会报错，这里 split(":",1) 只分割第一个冒号
-        #但是问题又来了，如果这一行中没有冒号怎么办呢？-->捕获异常，让程序继续执行
+        #但是问题又来了，如果这一行中没有冒号怎么办呢？-->捕获异常，让程序继续执行，哈
         try:
             (role,words) = e_line.split(':',1)
             print role,
